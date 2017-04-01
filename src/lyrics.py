ccabe27a0c86fc0964c07db9c55f619edde1a866
Nicolas Guichard <nicolas.guichard@ovh.fr>
2017-04-01 18:03:09 +0200

    - Correct an encoding problem with MetroLyrics lyrics  - More coherent HTTP request headers

--- src/lyrics.py
+++ src/lyrics.py
@@ -12,12 +12,13 @@ def get_lyrics(song_name):
 
     song_name += ' metrolyrics'
     name = quote_plus(song_name)
-    hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11'
-           '(KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-           'Accept-Language': 'en-US,en;q=0.8',
+    hdr = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0',
+           'Accept-Encoding': 'gzip, deflate, br',
+           'DNT': '1',
+           'UPGRADE-INSECURE-REQUESTS': '1',
            'Connection': 'keep-alive'}
 
-    url = 'http://www.google.com/search?q=' + name
+    url = 'https://www.google.com/search?q=' + name
 
     result = requests.get(url, headers=hdr).text
     link_start = result.find('http://www.metrolyrics.com')
@@ -25,20 +26,15 @@ def get_lyrics(song_name):
     if(link_start == -1):
         return("Lyrics not found on Metrolyrics")
         
-    link_end = result.find('html', link_start + 1)
-    link = result[link_start:link_end + 4]
+    link_end = result.find('html', link_start + 1) + 4
+    link = result[link_start:link_end]
 
-
-    lyrics_html = requests.get(link, headers={
-                               'User-Agent': 'Mozilla/5.0 (Macintosh; Intel'
-                               'Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, '
-                               'like Gecko) Chrome/55.0.2883.95 Safari/537.36'
-                               }
-                               ).text
+    r = requests.get(link, headers=hdr)
+    r.encoding = "utf-8"
+    lyrics_html = r.text
 
     soup = BeautifulSoup(lyrics_html, "lxml")
     raw_lyrics = (soup.findAll('p', attrs={'class': 'verse'}))
-    paras = []
     try:
         final_lyrics = unicode.join(u'\n', map(unicode, raw_lyrics))
     except NameError:
